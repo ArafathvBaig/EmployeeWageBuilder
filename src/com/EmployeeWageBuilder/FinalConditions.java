3d15408d0b4092c6136fb2704f0628c3c5912761
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-22 12:48:05 +0530

    Adding the Final Conditions File to UC6 Branch

--- src/com/EmployeeWageBuilder/FinalConditions.java
+++ src/com/EmployeeWageBuilder/FinalConditions.java
@@ -1,44 +1,65 @@
 package com.EmployeeWageBuilder;
 
+
 public class FinalConditions 
 {
-		public static final int IS_FULL_TIME = 1;
-		public static final int IS_PART_TIME = 2;
-		public static final int EMP_RATE_PER_HR = 20;
-		public static final int MAX_WORKING_DAYS = 20;
-		public static final int MAX_WORKING_HOURS = 100;
-		public static void main(String[] args) 
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HR = 20;
+	public static final int MAX_WORKING_DAYS = 20;
+	public static final int MAX_WORKING_HOURS = 100;
+	
+	int totalWorkingHrs = 0;
+	int totalWorkingDays = 0;
+	int empHrs = 0;
+	int dailyWage;
+	int totalWage;
+	int empCheck;
+	
+	public void working()
+	{
+		totalWorkingHrs=totalWorkingHrs+empHrs;
+		totalWorkingDays++;
+		if((totalWorkingHrs<=MAX_WORKING_HOURS) && (totalWorkingDays<=MAX_WORKING_DAYS))
 		{
-			int totalWorkingHrs = 0;
-			int totalWorkingDays = 0;
-			int empHrs = 0;
-			while((totalWorkingHrs<MAX_WORKING_HOURS) && (totalWorkingDays<MAX_WORKING_DAYS))
+			System.out.println("Randome Check is: "+empCheck);
+			System.out.println("Full Time Employee");
+			System.out.println("Total Working Hours = "+totalWorkingHrs);
+			System.out.println("Day = "+totalWorkingDays);
+			dailyWage = empHrs*EMP_RATE_PER_HR;
+			System.out.println("Employee Daily Wage is: "+dailyWage);
+			totalWage = totalWorkingHrs*EMP_RATE_PER_HR;
+			System.out.println("Total Wage is: "+totalWage);
+			System.out.println(" ");
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		FinalConditions obj1=new FinalConditions();
+		while((obj1.totalWorkingHrs<=MAX_WORKING_HOURS) && (obj1.totalWorkingDays<=MAX_WORKING_DAYS))
+		{
+			
+			obj1.empCheck =(int) ((Math.random())*10)%3;
+			//obj1.empCheck=1;
+			switch(obj1.empCheck)
 			{
-				int empCheck =(int) ((Math.random())*10)%3;
-				System.out.println(+empCheck);
-				switch(empCheck)
-				{
-				case IS_FULL_TIME:
-					System.out.println("Full Time Employee");
-					empHrs = 8;
-					break;
-				case IS_PART_TIME:
-					System.out.println("Part Time Employee");
-					empHrs = 4;
-					break;
-				default:
-					System.out.println("Employee is Absent");
-					empHrs = 0;
-					break;
-				}
-				totalWorkingHrs=totalWorkingHrs+empHrs;
-				totalWorkingDays++;
-				System.out.println("Total Working Hours = "+totalWorkingHrs);
-				System.out.println("Day = "+totalWorkingDays);
-				int dailyWage = empHrs*EMP_RATE_PER_HR;
-				System.out.println("Employee Daily Wage is: "+dailyWage);
-				int totalWage = totalWorkingHrs*EMP_RATE_PER_HR;
-				System.out.println("Total Wage is: "+totalWage);		
+			case IS_FULL_TIME:
+				obj1.empHrs = 8;
+				obj1.working();
+				break;
+				
+			case IS_PART_TIME:
+				obj1.empHrs = 4;
+				obj1.working();
+				break;
+				
+			default:
+				obj1.empHrs=0;
+				obj1.working();
+				break;
+				
 			}
+		}
 	}
 }
